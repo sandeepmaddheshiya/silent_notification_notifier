beb4598ac85a081aab44b38933cf56585b075e53
Sandeep Maddheshiya <113048079+Itz-Sandy@users.noreply.github.com>
2023-11-27 06:28:11 +0530

    second commit added play pause audio feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,14 @@
 import 'dart:async';
-import 'dart:developer';
 
-import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
 import 'package:notifications/notifications.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:wakelock/wakelock.dart';
-
-void backgroundTask() {
-  // Acquire a wakelock to keep the device awake during the background task
-  Wakelock.enable();
-
-  // Code to be executed in the background
-  // You can put your background task logic here
-
-  // Release the wakelock when the task is complete
-  Wakelock.disable();
-}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Request necessary permissions
-  await _requestPermissions();
-
-  // Initialize the wakelock plugin
   Wakelock.enable();
-
-  // Initialize the background task
-  await AndroidAlarmManager.initialize();
   runApp(MyApp());
-}
-
-Future<void> _requestPermissions() async {
-  await Permission.notification.request();
-  await Permission.ignoreBatteryOptimizations.request();
 }
 
 class MyApp extends StatefulWidget {
@@ -45,10 +18,11 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   Notifications? _notifications;
-  StreamSubscription<NotificationEvent>? _subscription;
   bool started = false;
   String? notificationTitle;
   String? notificationMessage;
+  bool musicPlaying = false;
+  StreamSubscription<NotificationEvent>? _subscription;
 
   @override
   void initState() {
@@ -56,7 +30,6 @@ class _MyAppState extends State<MyApp> {
     initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     startListening();
   }
@@ -67,47 +40,49 @@ class _MyAppState extends State<MyApp> {
       notificationMessage = event.message;
     });
 
-    log("Title: $notificationTitle");
-    log("Message: $notificationMessage");
-
-    // Check if the notification title matches the specified title
     if (notificationTitle == "Kunal Sms") {
-      // Play the custom ringtone
-      FlutterRingtonePlayer.play(
-        fromAsset: "lib/assets/music/ram_siya_ram_adipurush.mp3",
-        looping: true,
-        volume: 1,
-        asAlarm: true,
-      );
+      // Check if music is not already playing
+      if (!musicPlaying) {
+        // Play the custom ringtone
+        FlutterRingtonePlayer.play(
+          fromAsset: "lib/assets/music/ram_siya_ram_adipurush.mp3",
+          looping: true,
+          volume: 1,
+          asAlarm: true,
+        );
+        setState(() {
+          musicPlaying = true;
+        });
+      }
     }
-
-    log(event.toString());
   }
 
   void startListening() {
     _notifications = Notifications();
-    try {
-      _subscription = _notifications!.notificationStream!.listen(onData);
-      setState(() => started = true);
-    } on NotificationException catch (exception) {
-      log(exception.toString());
-    }
+    _notifications!.notificationStream!.listen(onData);
 
-    // Schedule the background task to run periodically
-    AndroidAlarmManager.periodic(
-        const Duration(minutes: 15), 0, backgroundTask);
+    // Set up a background task to stop the music after 15 seconds (adjust as needed)
+    Future.delayed(const Duration(seconds: 15), stopMusic);
+
+    setState(() => started = true);
   }
 
   void stopListening() {
     _subscription?.cancel();
+    stopMusic();
     setState(() {
       started = false;
       notificationTitle = null;
       notificationMessage = null;
     });
+  }
 
-    // Cancel the background task when not needed
-    AndroidAlarmManager.cancel(0);
+  void stopMusic() {
+    // Stop the music
+    FlutterRingtonePlayer.stop();
+    setState(() {
+      musicPlaying = false;
+    });
   }
 
   @override
@@ -130,7 +105,7 @@ class _MyAppState extends State<MyApp> {
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: started ? stopListening : startListening,
-          tooltip: 'Start/Stop sensing',
+          tooltip: started ? 'Stop sensing' : 'Start sensing',
           child:
               started ? const Icon(Icons.stop) : const Icon(Icons.play_arrow),
         ),
@@ -140,7 +115,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   void dispose() {
-    // Release the wakelock when the app is disposed
     Wakelock.disable();
     super.dispose();
   }
